a213579bc9230d2f4484c4ccb0b8d1552016d083
Tai Smith <imadeanomelet@gmail.com>
2020-10-26 01:11:03 -0500

    Moved the real files to the clone-clone

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// TODO: add code here
+window.addEventListener('load', function(){
+    console.log('Loaded.');
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response){
+        response.json().then(function(json) {
+            console.log(json);
+                let container = document.getElementById('container');
+
+                for (let i = 0; i < 7; i++){
+                    container.innerHTML += `
+                        <div class = "astronaut">
+                            <div class = "bio">
+                                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                                    <ul>
+                                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                        <li>Active: ${json[i].active}</li>
+                                        <li>Skills: ${json[i].skills}</li>
+                                    </ul>
+                            </div>
+                            <img class="avatar" src = ${json[i].picture}></img>
+                        </div>`;
+                };
+
+            });
+        });
+});
